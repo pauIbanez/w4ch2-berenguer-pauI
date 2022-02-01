88185d5dda1566580586b1ded692a709ef1dc21c
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-01 21:25:10 +0100

    Fixed space rendering in UsedLetters function

--- src/components/UsedLetters/UsedLetters.js
+++ src/components/UsedLetters/UsedLetters.js
@@ -5,13 +5,15 @@ const UsedLetters = ({ letters, classUsedLetter }) => {
     <section className="used-letters-container">
       <h2>Used letters</h2>
       <ul className="used-letters">
-        {letters.map((letter, i, letters) =>
+        {letters.map((letter, index, letters) =>
           i !== letters.length - 1 ? (
-            <li
-              className={classUsedLetter}
-            >{`${letter.toUpperCase()},&nbsp;`}</li>
+            <li key={index} className={classUsedLetter}>
+              {letter.toUpperCase()}, &nbsp;
+            </li>
           ) : (
-            <li className={classUsedLetter}>{`${letter.toUpperCase()}`}</li>
+            <li key={index} className={classUsedLetter}>
+              {letter.toUpperCase()}
+            </li>
           )
         )}
       </ul>
