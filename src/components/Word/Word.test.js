61b298995292a1aa08615fd0995886ea79675333
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-01 20:47:33 +0100

    Added seccond test for Word.js

--- src/components/Word/Word.test.js
+++ src/components/Word/Word.test.js
@@ -17,4 +17,25 @@ describe("Given Word", () => {
       expect(elements).toBe(expectedLenght);
     });
   });
+
+  describe("When it's invoked passing 'testing' and ['t', 'e'] as lettersPlayed", () => {
+    test("Then it should render 4 elements with className 'guess-letter empty' and 2 with the text 'T'", () => {
+      const word = "testing";
+      const playedLetters = ["t", "e"];
+      const expectedEmptyLength = 4;
+      const expectedNotEmptyLength = 2;
+
+      render(<Word word={word} lettersPlayed={playedLetters} />);
+      const emptyElements = screen
+        .queryAllByText("")
+        .filter((element) => element.className === "guess-letter empty").length;
+
+      const notEmptyElements = screen
+        .queryAllByText("T")
+        .filter((element) => element.className === "guess-letter").length;
+
+      expect(emptyElements).toBe(expectedEmptyLength);
+      expect(notEmptyElements).toBe(expectedNotEmptyLength);
+    });
+  });
 });
