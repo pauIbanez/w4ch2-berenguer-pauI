5ab33f1ebf68dc917c47db9696fc04c875b3bc5f
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-01 20:56:14 +0100

    Generated letter as a component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Letter from "./components/Letter/Letter";
 import Word from "./components/Word/Word";
 
 function App() {
@@ -30,85 +31,7 @@ function App() {
       </section>
       <section class="game-result">You're dead!</section>
       <ul class="letters">
-        <li class="letter">
-          <a href="a">A</a>
-        </li>
-        <li class="letter">
-          <a href="b">B</a>
-        </li>
-        <li class="letter">
-          <a href="c">C</a>
-        </li>
-
-        <li class="letter">
-          <a href="e">E</a>
-        </li>
-        <li class="letter">
-          <a href="f">F</a>
-        </li>
-        <li class="letter">
-          <a href="g">G</a>
-        </li>
-        <li class="letter">
-          <a href="h">H</a>
-        </li>
-        <li class="letter">
-          <a href="i">I</a>
-        </li>
-        <li class="letter">
-          <a href="j">J</a>
-        </li>
-        <li class="letter">
-          <a href="k">K</a>
-        </li>
-        <li class="letter">
-          <a href="l">L</a>
-        </li>
-        <li class="letter">
-          <a href="m">M</a>
-        </li>
-        <li class="letter">
-          <a href="n">N</a>
-        </li>
-        <li class="letter">
-          <a href="ñ">Ñ</a>
-        </li>
-        <li class="letter">
-          <a href="o">O</a>
-        </li>
-        <li class="letter">
-          <a href="p">P</a>
-        </li>
-        <li class="letter">
-          <a href="q">Q</a>
-        </li>
-        <li class="letter">
-          <a href="r">R</a>
-        </li>
-        <li class="letter">
-          <a href="s">S</a>
-        </li>
-        <li class="letter">
-          <a href="t">T</a>
-        </li>
-        <li class="letter">
-          <a href="u">U</a>
-        </li>
-        <li class="letter">
-          <a href="v">V</a>
-        </li>
-        <li class="letter">
-          <a href="w">W</a>
-        </li>
-        <li class="letter">
-          <a href="x">X</a>
-        </li>
-        <li class="letter">
-          <a href="y">Y</a>
-        </li>
-        <li class="letter">
-          <a href="z">Z</a>
-        </li>
+        <Letter letter="A" />
       </ul>
     </>
   );
