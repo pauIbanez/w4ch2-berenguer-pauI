a549393618f3a33a099065cf858900796a2c2bbb
Berenguer Pou <berenguer.pou@gmail.com>
2022-02-02 08:03:03 +0100

    sass: added som new styles to components

--- src/components/UsedLetters/UsedLetters.js
+++ src/components/UsedLetters/UsedLetters.js
@@ -3,7 +3,7 @@ import "./UsedLetters_style.scss";
 const UsedLetters = ({ letters }) => {
   return (
     <section className="used-letters-container">
-      <h2>Used letters</h2>
+      <h2 className="used-letters__title">Used letters</h2>
       <ul className="used-letters">
         {letters.map((letter, index, letters) =>
           index !== letters.length - 1 ? (
